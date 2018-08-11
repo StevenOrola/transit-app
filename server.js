41c831de5d247024ce3573fd36b64892861507b2
Steven Orola <stevenorola@gmail.com>
2018-08-11 11:21:04 -0700

    queries not working as expected in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 var Sequelize = require('sequelize');
 var Op = Sequelize.Op;
 var sequelize = new Sequelize('db', null, null, {
@@ -11,9 +12,17 @@ var sequelize = new Sequelize('db', null, null, {
 });
 const port = process.env.PORT || 5000;
 
-var stops = sequelize.query("SELECT * FROM stop_times WHERE stop_id = 772 AND " +
+var sqlQuery = "SELECT * FROM stop_times WHERE stop_id = 772 AND " +
  "arrival_time > strftime('%H:%M:%S', 'now', 'localtime', '+3 minutes') " +
- "ORDER BY arrival_time LIMIT 10;");
+ "ORDER BY arrival_time LIMIT 10;"
+var stops = sequelize.query(sqlQuery, {
+	 raw: true 
+ });
+ 
+ //.then(function(row){stops.push(row);console.log(stops);});
+ 
+
+
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: stops, msg: 'hi' });
